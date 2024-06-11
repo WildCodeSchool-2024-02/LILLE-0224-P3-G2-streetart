0ea2a1d7c15d8ee801f9ab967ba026c359babd16
eloregnier <elodie.regnier.92@gmail.com>
2024-06-11 11:41:11 +0200

    NavBar Aside Desktop version OK

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBarBottom from "./components/NavBarBottom/NavBarBottom";
 import { BurgerMenuProvider } from "./contexts/BurgerMenuContext";
+import AsideMenu from "./components/AsideMenu/AsideMenu";
 
 function App() {
   return (
     <BurgerMenuProvider>
-      <main className="container">
-        <NavBarBottom />
+      <main className="main-container">
+        <AsideMenu />
         <Outlet />
       </main>
+      <NavBarBottom />
     </BurgerMenuProvider>
   );
 }
---
+++ client/src/components/AsideMenu/AsideMenu.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./AsideMenu.css";
+
+function AsideMenu() {
+  return (
+    <aside className="aside-menu-container">
+      <div className="aside-menu" role="presentation">
+        <div className="aside-menu-top">
+          <Link to="/">Accueil</Link>
+          <Link to="/oeuvres">Les oeuvres</Link>
+          <Link to="/carte">Roadmap</Link>
+          <Link to="/classement">Classement</Link>
+          <Link to="/profil">Profil</Link>
+        </div>
+        <div className="aside-menu-bottom">
+          <Link to="/a-propos">A propos</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+export default AsideMenu;
