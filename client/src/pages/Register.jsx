0cb7b9428ca134ed4f09bd64027831ec5fcb49a7
eloregnier <elodie.regnier.92@gmail.com>
2024-06-11 21:25:34 +0200

    Register page to validate with the team

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,51 @@
+import "./styles/Register.css";
+
 function Register() {
   return (
-    <>
-      <p>Register</p>
-      <p>Coucou</p>
-    </>
+    <div className="register-container">
+      <h2 className="register-title">Inscription</h2>
+      <form className="register-formulaire">
+        <div className="field">
+          <input type="text" className="input" placeholder="Pseudo" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="text" className="input" placeholder="Mot de passe" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="text" className="input" placeholder="Nom" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="text" className="input" placeholder="Prénom" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            className="input"
+            placeholder="Date de naissance"
+          />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="text" className="input" placeholder="Email" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="text" className="input" placeholder="Code Postal" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="text" className="input" placeholder="Ville" />
+          <div className="line" />
+        </div>
+        <button type="button" className="btn">
+          M'inscrire
+        </button>
+      </form>
+    </div>
   );
 }
 
