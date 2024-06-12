411afa706412d18c72d2952543e9f78fad69ba5d
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-06-12 12:27:57 +0200

    update artworks details mobile and desktop version

--- client/src/components/OthersArtworks/OthersArtworks.jsx
+++ client/src/components/OthersArtworks/OthersArtworks.jsx
@@ -3,7 +3,41 @@ import "./OthersArtworks.css";
 function OthersArtworks() {
   return (
     <div className="others-artworks">
-      <h3 className="others-title">Oeuvres similaires </h3>
+      <h3 className="others-title">À découvrir </h3>
+      <div className="img-othersartworks">
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+        <img
+          src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+          alt="oeuvre de street art"
+        />
+      </div>
     </div>
   );
 }
