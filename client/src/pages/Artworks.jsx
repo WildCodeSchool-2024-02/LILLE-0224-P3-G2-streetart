012bb62f188c8508364d39cfcb6e802786fa04d0
iSayZ <estrine.alexis@gmail.com>
2024-06-12 10:13:50 +0200

    Adding artwork page, and fixing desktop template

--- client/src/pages/Artworks.jsx
+++ client/src/pages/Artworks.jsx
@@ -1,9 +1,26 @@
+import ArtworkCard from "../components/ArtworkCard/ArtworkCard";
+import "./styles/Artworks.css"
+
 function Artworks() {
+  
   return (
-    <>
-      <p>Artworks</p>
-      <p>Coucou</p>
-    </>
+    <div className="artworks-container">
+      <ArtworkCard picture="/assets/images/artworks/artwork1.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork2.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork3.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork4.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork5.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork6.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork7.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork8.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork1.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork2.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork3.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork4.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork1.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork2.png" />
+      <ArtworkCard picture="/assets/images/artworks/artwork3.png" />
+    </div>
   );
 }
 
