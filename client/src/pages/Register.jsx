c4718daf36afeb58e54bbe0d0dc62dcec513b05d
eloregnier <elodie.regnier.92@gmail.com>
2024-06-12 12:05:51 +0200

    F10_1 Inscription Page OK step1 - have to add required on fields on F10_2

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,16 +3,20 @@ import "./styles/Register.css";
 function Register() {
   return (
     <div className="register-container">
-      <h2 className="register-title">Inscription</h2>
+      <div className="inscription-img-container">
+        <img
+          className="inscription-img"
+          src="/public/assets/images/inscription-img.png"
+          alt="street art représentant un DJ"
+        />
+      </div>
       <form className="register-formulaire">
+        <h2 className="register-title">Inscription</h2>
         <div className="field">
           <input type="text" className="input" placeholder="Pseudo" />
           <div className="line" />
         </div>
-        <div className="field">
-          <input type="text" className="input" placeholder="Mot de passe" />
-          <div className="line" />
-        </div>
+
         <div className="field">
           <input type="text" className="input" placeholder="Nom" />
           <div className="line" />
@@ -21,24 +25,29 @@ function Register() {
           <input type="text" className="input" placeholder="Prénom" />
           <div className="line" />
         </div>
+
         <div className="field">
-          <input
-            type="text"
-            className="input"
-            placeholder="Date de naissance"
-          />
+          <input type="email" className="input" placeholder="Email" />
           <div className="line" />
         </div>
         <div className="field">
-          <input type="text" className="input" placeholder="Email" />
-          <div className="line" />
-        </div>
-        <div className="field">
-          <input type="text" className="input" placeholder="Code Postal" />
+          <input type="number" className="input" placeholder="Code Postal" />
           <div className="line" />
         </div>
         <div className="field">
           <input type="text" className="input" placeholder="Ville" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input type="password" className="input" placeholder="Mot de passe" />
+          <div className="line" />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            className="input"
+            placeholder="Confirmer le mot de passe"
+          />
           <div className="line" />
         </div>
         <button type="button" className="btn">
