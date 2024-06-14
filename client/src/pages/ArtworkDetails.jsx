7a58c0d791cd43d1fc06be979d430c9696b5ac43
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-06-14 22:10:08 +0200

    update artwork details on mobile and desktop version

--- client/src/pages/ArtworkDetails.jsx
+++ client/src/pages/ArtworkDetails.jsx
@@ -5,32 +5,48 @@ function ArtworkDetails() {
   return (
     <>
       <div className="artworkdetails-container">
-        <div className="artwork-img">
-          <h2 className="artwork-title">CHAT JAUNE</h2>
-          <img
-            src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
-            alt="oeuvre de street art"
-          />
-        </div>
-        <img
-          src="https://th.bing.com/th/id/R.d25b9dc7fecaa168cd8e12a5cb5e8927?rik=KoGEJmaj5qXrNA&riu=http%3a%2f%2ffr.maps-lille.com%2fimg%2f1200%2flille-carte-de-transport.jpg&ehk=R%2f6qyqwiZIr0IdrPd4bX4UEdUGmz37k53g5Vf20p%2feI%3d&risl=&pid=ImgRaw&r=0"
-          alt="carte"
-          className="map"
-        />
-
-        <div className="about-user-artwork">
-          <p>Ajoutée par :</p>
-          <img
-            className="user-img-artwork"
-            src="https://www.radiofrance.fr/s3/cruiser-production/2022/10/4221556a-d1ba-4f34-9ec4-2819e102ea57/1200x680_pierre-niney.jpg"
-            alt="avatar de la personne qui a ajouté l'oeuvre"
-          />
-          <div>
-            <p>Pierrot</p>
-            <p>lvl 56</p>
+        <div className="artworkdetails-info">
+          <div className="artworksdetails-all-info">
+            <div className="artwork-img">
+              <h2 className="artwork-title">CHAT JAUNE</h2>
+              <img
+                src="https://www.lescachotteriesdelille.com/wp-content/uploads/2023/12/Monsieur-Chat_640x479.jpg"
+                alt="oeuvre de street art"
+              />
+            </div>
+            <div className="flex-right">
+              <div className="map-artworkdetails">
+                <img
+                  src="https://th.bing.com/th/id/R.d25b9dc7fecaa168cd8e12a5cb5e8927?rik=KoGEJmaj5qXrNA&riu=http%3a%2f%2ffr.maps-lille.com%2fimg%2f1200%2flille-carte-de-transport.jpg&ehk=R%2f6qyqwiZIr0IdrPd4bX4UEdUGmz37k53g5Vf20p%2feI%3d&risl=&pid=ImgRaw&r=0"
+                  alt="carte"
+                  className="map"
+                />
+              </div>
+              <div className="info-artworkdetails">
+                <div className="about-artwork">
+                  <div className="about-user-artwork">
+                    <p>Ajoutée par :</p>
+                    <img
+                      className="user-img-artwork"
+                      src="https://www.radiofrance.fr/s3/cruiser-production/2022/10/4221556a-d1ba-4f34-9ec4-2819e102ea57/1200x680_pierre-niney.jpg"
+                      alt="avatar de la personne qui a ajouté l'oeuvre"
+                    />
+                    <div>
+                      <p>Pierrot</p>
+                      <p>lvl 56</p>
+                    </div>
+                  </div>
+                  <p className="about-artwork">À Lille, le 03/06/2024</p>
+                </div>
+              </div>
+              <div className="report-artworkdetails">
+                <button type="button" className="report-btn">
+                  Signaler la disparition de l'oeuvre
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        <p className="about-artwork">À Lille, le 03/06/2024</p>
       </div>
       <OthersArtworks />
     </>
