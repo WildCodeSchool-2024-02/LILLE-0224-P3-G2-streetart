6c9cc8795e305e463f254918855a588df90fa771
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-06-15 00:26:33 +0200

    profile mobile and desktop version

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,7 @@ function Profile() {
             />
             <p className="pseudo-profile">pseudo</p>
           </div>
-          <div className="points-edit">
+          <div className="points-edit-mobile">
             <img
               className="edit-profile"
               src="/assets/images/icons/edit.png"
@@ -26,18 +26,39 @@ function Profile() {
             </div>
           </div>
         </div>
-        <div className="info-profile">
-          <p>François Civil</p>
-          <p>14/07/1990</p>
-          <p>françois.civil@gmail.com</p>
-          <p>770012 Paris</p>
-          <p>**********</p>
+        <div className="info-desktop">
+          <div className="info-profile">
+            <p>François Civil</p>
+            <p>14/07/1990</p>
+            <p>françois.civil@gmail.com</p>
+            <p>770012 Paris</p>
+            <p>**********</p>
+          </div>
+          <div className="points-edit-desktop">
+            <img
+              className="edit-profile-desktop"
+              src="/assets/images/icons/edit.png"
+              alt="crayon pour modifier les infos du profil"
+            />
+            <div className="level-points">
+              <p>LVL 87</p>
+              <p>173 points</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="my-artworks">
-        <ArtworkCard picture="/assets/images/artworks/artwork6.png" />
-        <ArtworkCard picture="/assets/images/artworks/artwork7.png" />
-        <ArtworkCard picture="/assets/images/artworks/artwork8.png" />
+        <div className="title-my-artworks">
+          <h2>Mes oeuvres</h2>
+        </div>
+        <div className="artworks-profile">
+          <ArtworkCard picture="/assets/images/artworks/artwork6.png" />
+          <ArtworkCard picture="/assets/images/artworks/artwork7.png" />
+          <ArtworkCard picture="/assets/images/artworks/artwork8.png" />
+          <div className="artwork4">
+            <ArtworkCard picture="/assets/images/artworks/artwork5.png" />
+          </div>
+        </div>
       </div>
     </div>
   );
