dd188c7425100c3fbe483cdc141c129bf727aa20
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-06-15 07:52:18 +0200

    update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,52 +1,76 @@
 import "./styles/Profile.css";
 import ArtworkCard from "../components/ArtworkCard/ArtworkCard";
 
+const profiles = [
+  {
+    picture:
+      "https://www.parismatch.com/lmnr/var/pm/public/media/image/2022/02/28/16/Francois-Civil.jpg?VersionId=3NZNfTbY_QeYa6WOzqlWurka0zzg9TGn",
+    firstName: "François",
+    lastName: "Civil",
+    pseudo: "pseudo",
+    email: "françois.civil@gmail.com",
+    password: "**********",
+    postcode: "770012 ",
+    city: "Paris",
+    level: "87",
+    points: "187",
+  },
+];
+
 function Profile() {
   return (
     <div className="profile-container">
-      <div className="my-profile">
-        <div className="top-profile">
-          <div className="img-profile">
-            <img
-              className="image-profile"
-              src="https://www.parismatch.com/lmnr/var/pm/public/media/image/2022/02/28/16/Francois-Civil.jpg?VersionId=3NZNfTbY_QeYa6WOzqlWurka0zzg9TGn"
-              alt="profil"
-            />
-            <p className="pseudo-profile">pseudo</p>
+      {profiles.map((profile) => (
+        <div
+          className="my-profile"
+          key={`${profile.firstName} {profile.lastName}`}
+        >
+          <div className="top-profile">
+            <div className="img-profile">
+              <img
+                className="image-profile"
+                src="https://www.parismatch.com/lmnr/var/pm/public/media/image/2022/02/28/16/Francois-Civil.jpg?VersionId=3NZNfTbY_QeYa6WOzqlWurka0zzg9TGn"
+                alt="profil"
+              />
+              <p className="pseudo-profile">{profile.pseudo}</p>
+            </div>
+            <div className="points-edit-mobile">
+              <img
+                className="edit-profile"
+                src="/assets/images/icons/edit.png"
+                alt="crayon pour modifier les infos du profil"
+              />
+              <div className="level-points">
+                <p>LVL {profile.level}</p>
+                <p>{profile.points} points</p>
+              </div>
+            </div>
           </div>
-          <div className="points-edit-mobile">
-            <img
-              className="edit-profile"
-              src="/assets/images/icons/edit.png"
-              alt="crayon pour modifier les infos du profil"
-            />
-            <div className="level-points">
-              <p>LVL 87</p>
-              <p>173 points</p>
+          <div className="info-desktop">
+            <div className="info-profile">
+              <p>
+                {profile.firstName} {profile.lastName}
+              </p>
+              <p>{profile.email}</p>
+              <p>
+                {profile.postcode} {profile.city}
+              </p>
+              <p>{profile.password}</p>
+            </div>
+            <div className="points-edit-desktop">
+              <img
+                className="edit-profile-desktop"
+                src="/assets/images/icons/edit.png"
+                alt="crayon pour modifier les infos du profil"
+              />
+              <div className="level-points">
+                <p>LVL 87</p>
+                <p>173 points</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="info-desktop">
-          <div className="info-profile">
-            <p>François Civil</p>
-            <p>14/07/1990</p>
-            <p>françois.civil@gmail.com</p>
-            <p>770012 Paris</p>
-            <p>**********</p>
-          </div>
-          <div className="points-edit-desktop">
-            <img
-              className="edit-profile-desktop"
-              src="/assets/images/icons/edit.png"
-              alt="crayon pour modifier les infos du profil"
-            />
-            <div className="level-points">
-              <p>LVL 87</p>
-              <p>173 points</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
       <div className="my-artworks">
         <div className="title-my-artworks">
           <h2>Mes oeuvres</h2>
