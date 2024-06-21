a9cdf83a070d2e06e3ef770988b6743da6ea9bd8
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-06-21 16:44:36 +0200

    fix artwork details

--- client/src/pages/ArtworkDetails.jsx
+++ client/src/pages/ArtworkDetails.jsx
@@ -1,95 +1,104 @@
 import { useLoaderData } from "react-router-dom";
 import { useState } from "react";
-import OthersArtworks from "../components/OthersArtworks/OthersArtworks";
 import "./styles/ArtworkDetails.css";
+import OthersArtworks from "../components/OthersArtworks/OthersArtworks";
+import RoadMapDetails from "../components/RoadMapDetails/RoadMapDetails";
 
 function ArtworkDetails() {
-
   const artwork = useLoaderData();
 
-  const [city, setCity] = useState(null)
+  const [city, setCity] = useState(null);
 
   // StreetMap API for get the adress with latitude and longitude
   const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${artwork.latitude}&lon=${artwork.longitude}&zoom=10&addressdetails=1`;
 
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       if (data.address) {
-        setCity(data.address.city || data.address.town || data.address.village)
+        setCity(data.address.city || data.address.town || data.address.village);
       } else {
-        setCity("inconnu")
+        setCity("inconnu");
       }
-    })
+    });
 
   // Function for open the picture on full screen
   const fullScreenPicture = () => {
-    const image = document.getElementById('fullscreen-image');
+    const image = document.getElementById("fullscreen-image");
     if (image.requestFullscreen) {
       image.requestFullscreen();
-    } else if (image.mozRequestFullScreen) { // Firefox
+    } else if (image.mozRequestFullScreen) {
+      // Firefox
       image.mozRequestFullScreen();
-    } else if (image.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    } else if (image.webkitRequestFullscreen) {
+      // Chrome, Safari and Opera
       image.webkitRequestFullscreen();
-    } else if (image.msRequestFullscreen) { // IE/Edge
+    } else if (image.msRequestFullscreen) {
+      // IE/Edge
       image.msRequestFullscreen();
     }
   };
 
   return (
-    <>
-      <div className="artworkdetails-container">
-        <div className="artworksdetails-all-info">
-          <div className="artwork-img">
-            <h2 className="artwork-title">{artwork.title}</h2>
-            <button
-              type="button"
-              onClick={fullScreenPicture}
-              style={{ border: 'none', padding: 0, background: 'none', cursor: 'pointer' }}
-              aria-label="Voir l'image en plein écran"
-            >
+    <div className="artworkdetails-container">
+      <div className="artworkdetails-right-container">
+        <div className="artworkdetails-img-container">
+          <h2 className="artwork-title">{artwork.title}</h2>
+          <button
+            type="button"
+            onClick={fullScreenPicture}
+            style={{
+              border: "none",
+              padding: 0,
+              background: "none",
+              cursor: "pointer",
+            }}
+            aria-label="Voir l'image en plein écran"
+          >
+            <div className="artworkdetails-img">
               <img
                 id="fullscreen-image"
                 src={artwork.picture}
                 alt={artwork.title}
               />
-            </button>
+            </div>
+          </button>
+        </div>
+
+        <div className="artworkdetails-left-container">
+          <div className="artworkdetails-map">
+            <RoadMapDetails />
           </div>
-          <div className="flex-right">
-            <div className="map-artworkdetails">
-              <img
-                src="https://th.bing.com/th/id/R.d25b9dc7fecaa168cd8e12a5cb5e8927?rik=KoGEJmaj5qXrNA&riu=http%3a%2f%2ffr.maps-lille.com%2fimg%2f1200%2flille-carte-de-transport.jpg&ehk=R%2f6qyqwiZIr0IdrPd4bX4UEdUGmz37k53g5Vf20p%2feI%3d&risl=&pid=ImgRaw&r=0"
-                alt="carte"
-                className="map"
-              />
-            </div>
-            <div className="info-artworkdetails">
-              <div className="about-artwork">
-                <div className="about-user-artwork">
-                  <p>Ajoutée par :</p>
-                  <img
-                    className="user-img-artwork"
-                    src="https://www.radiofrance.fr/s3/cruiser-production/2022/10/4221556a-d1ba-4f34-9ec4-2819e102ea57/1200x680_pierre-niney.jpg"
-                    alt="avatar de la personne qui a ajouté l'oeuvre"
-                  />
-                  <div>
-                    <p>{artwork.pseudo}</p>
-                    <p>lvl 56</p>
-                  </div>
-                </div>
-                <p className="about-artwork">À {city}, le {artwork.date_creation}</p>
+
+          <div className="artworkdetails-info">
+            <div className="artworkdetails-user">
+              <div className="artworkdetails-addby">
+                <p>Ajoutée par :</p>
+                <p>{artwork.pseudo}</p>
               </div>
+              <img
+                className="user-img-artwork"
+                src="https://www.radiofrance.fr/s3/cruiser-production/2022/10/4221556a-d1ba-4f34-9ec4-2819e102ea57/1200x680_pierre-niney.jpg"
+                alt="avatar de la personne qui a ajouté l'oeuvre"
+              />
+              <p>badge</p>
             </div>
-            <div className="report-artworkdetails">
-              <button type="button" className="report-btn">
-                Signaler la disparition de l'oeuvre
-              </button>
-            </div>
+
+            <p className="about-artwork">
+              À {city}, le {artwork.date_creation}
+            </p>
+          </div>
+
+          <div className="artworkdetails-reportbtn">
+            <button type="button" className="report-btn">
+              Signaler la disparition de l'oeuvre
+            </button>
           </div>
         </div>
       </div>
+
       <OthersArtworks />
-    </>
+    </div>
   );
 }
 
