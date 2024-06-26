4c95b93168ce4d606ca341909e12ec06486a2d6c
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-06-26 13:15:45 +0200

    fix website

--- client/src/pages/ValidationArtwork.jsx
+++ client/src/pages/ValidationArtwork.jsx
@@ -1,64 +1,57 @@
-import "./styles/ValidationArtwork.css"
-import "../components/NewArtwork/FormArtwork/FormArtwork.css"
+import "./styles/ValidationArtwork.css";
+import "../components/NewArtwork/FormArtwork/FormArtwork.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useNewArtwork } from "../contexts/NewArtworkContext";
 import TopBar from "../components/TopBar/TopBar";
-import NavBarBottom from "../components/NavBarBottom/NavBarBottom"
-import { BurgerMenuProvider } from "../contexts/BurgerMenuContext";
 
 function ValidationArtwork() {
+  const navigate = useNavigate();
+  const { title, image } = useNewArtwork();
+  const [loader, setLoader] = useState(true);
 
-    const navigate = useNavigate()
-    const { title, image } = useNewArtwork();
-    const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 5000);
+  }, []);
 
-    useEffect(
-        () => {
-            setTimeout(() => {
-                setLoader(false)
-            }, 5000);
-        }, []
-    );
+  const navigateToHome = () => {
+    navigate("/");
+  };
 
-    const navigateToHome = () => {
-        navigate("/")
-    }
-
-    return (
-        <>
-            <TopBar title="Spot Lille Art" />
-            {loader ?            
-                <div className="loader-container">
-                    <span className="loader" />
-                </div>
-                :
-                <div className="validation-artwork-container">
-                                    <div className="validation-artwork">
-                    <div className="camera-picture">
-                        <img src={image} alt={title} />
-                    </div>
-                    <div className="validation-info">
-                    <p>
-                        Ton oeuvre <span className="focus-text">{title}</span> a bien été postée, elle doit maintenant être validée par un administrateur afin de figurer sur le site.
-                    </p>
-                    <br/>
-                    <p className="focus-text">
-                        Tes points te seront attribués lors de la validation.
-                    </p>
-                    </div>
-                    <button type="button" className="btn" onClick={navigateToHome}>Revenir à l'accueil</button>
-                    </div>
-                </div>
+  return (
+    <>
+      <TopBar title="Spot Lille Art" />
+      {loader ? (
+        <div className="loader-container">
+          <span className="loader" />
+        </div>
+      ) : (
+        <div className="validation-artwork-container">
+          <div className="validation-artwork">
+            <div className="camera-picture">
+              <img src={image} alt={title} />
             </div>
-            <BurgerMenuProvider>
-                <NavBarBottom />
-            </BurgerMenuProvider>
-
-            }
-
-        </>
-    )
+            <div className="validation-info">
+              <p>
+                Ton oeuvre <span className="focus-text">{title}</span> a bien
+                été postée, elle doit maintenant être validée par un
+                administrateur afin de figurer sur le site.
+              </p>
+              <br />
+              <p className="focus-text">
+                Tes points te seront attribués lors de la validation.
+              </p>
+            </div>
+            <button type="button" className="btn" onClick={navigateToHome}>
+              Revenir à l'accueil
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ValidationArtwork;
