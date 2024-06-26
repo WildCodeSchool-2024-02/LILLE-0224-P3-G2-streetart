f89fe63fa8e0776a849e59daad4251d6087eb36a
iSayZ <estrine.alexis@gmail.com>
2024-06-26 22:43:59 +0200

    Authentification road

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -2,44 +2,34 @@ import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { createContext, useContext, useState, useMemo } from "react";
 
-  const AuthContext = createContext();
-  export function AuthProvider({
-    children,
-  }) {
+const AuthContext = createContext();
 
-    const [auth, setAuth] = useState({
-        token: localStorage.getItem('authToken'),
-        account: JSON.parse(localStorage.getItem('account'))
-      });
+export function AuthProvider({ children }) {
+  const [auth, setAuth] = useState({
+    token: localStorage.getItem('authToken'),
+    account: JSON.parse(localStorage.getItem('account')),
+  });
 
-      const navigate = useNavigate();
+  const navigate = useNavigate();
 
-      const logout = () => {
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('account');
-        setAuth({ token: null, account: null });
-        navigate('/connexion');
-      };
-
-      const contextValue = useMemo(
-        () => ({
-          auth,
-          setAuth,
-          logout,
-        })  
-        [auth]
-      );    
-
-      return (
-          <AuthContext.Provider value={contextValue}>
-              {children}
-          </AuthContext.Provider>
-      )
-  }
-
-  AuthProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('account');
+    setAuth({ token: null, account: null });
+    navigate('/connexion');
   };
 
-  export const useAuth =
-    () => useContext(AuthContext);
+  const contextValue = useMemo(() => ({ auth, setAuth, logout }), [auth]);
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useAuth = () => useContext(AuthContext);
