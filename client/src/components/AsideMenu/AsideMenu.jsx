66190fc134bfe6729ae87887316c70e9f641d32e
iSayZ <estrine.alexis@gmail.com>
2024-06-27 11:28:14 +0200

    Add MenuList for disconnect

--- client/src/components/AsideMenu/AsideMenu.jsx
+++ client/src/components/AsideMenu/AsideMenu.jsx
@@ -1,10 +1,7 @@
 import { Link } from "react-router-dom";
 import "./AsideMenu.css";
-import { useAuth } from "../../contexts/AuthContext";
 
 function AsideMenu() {
-
-  const { auth, logout } = useAuth();
 
   return (
     <aside className="aside-menu-container">
@@ -14,13 +11,6 @@ function AsideMenu() {
           <Link to="/oeuvres">Les oeuvres</Link>
           <Link to="/carte">Roadmap</Link>
           <Link to="/classement">Classement</Link>
-          {
-            auth.account &&
-            <>
-          <Link to={`/profil/${auth.account.id_member}`}>Profil</Link>
-          <button type="button" onClick={logout}>Deconnexion</button>
-            </>
-          }
         </div>
         <div className="aside-menu-bottom">
           <Link to="/a-propos">A propos</Link>
--- client/src/components/TopBar/ProfileDetails/ProfileDetails.jsx
+++ client/src/components/TopBar/ProfileDetails/ProfileDetails.jsx
@@ -1,14 +1,19 @@
 import "./ProfileDetails.css"
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useAuth } from "../../../contexts/AuthContext";
+import MenuList from "../../MenuList/MenuList";
 
 function ProfileDetails() {
 
     const { auth } = useAuth();
+    const [ openMenuList, setOpenMenuList ] = useState(false);
+
+    const handleClick = () => {
+        setOpenMenuList(!openMenuList)
+    }
 
     return (
-        <div className="profile-details">
-            <Link to={`/profil/${auth.account.id_member}`}>     
+        <button type="button" onClick={handleClick} className="profile-details" id={openMenuList && "is-open"}>
                 <div className="profile-picture">
                     {auth.account.avatar ?
                         <img src={auth.account.avatar} alt={`Avatar de ${auth.account.pseudo}`} />
@@ -16,11 +21,9 @@ function ProfileDetails() {
                         <img src="/assets/images/icons/profile.png" alt={`Avatar de ${auth.account.pseudo}`} />
                     }
                 </div>
-            </Link>
-            <Link to={`/profil/${auth.account.id_member}`}> 
                 <p className="profile-info"><span id="pseudo">{auth.account.pseudo}</span>{auth.account.points}<span><img src="/assets/images/icons/coin.png" alt="Jetons" id="coin" /></span></p>
-            </Link>
-        </div>
+        { openMenuList && <MenuList setOpenMenuList={setOpenMenuList} /> }    
+        </button>
     )
 }
 
